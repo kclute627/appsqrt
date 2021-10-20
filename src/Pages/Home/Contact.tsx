82d81c3581c84fe99124909b21844efc260e9ab8
Kyle C <kyclutter@gmail.com>
2021-10-20 12:41:18 -0500

    updates

--- src/Pages/Home/Contact.tsx
+++ src/Pages/Home/Contact.tsx
@@ -7,58 +7,60 @@ function Contact() {
       <div className="home_contact-left"></div>
       <div className="home_contact-right">
         <div className="form">
-            <h2>We Can Do This Together.</h2>
+          <h2>We Can Do This Together.</h2>
           <div className="form-field">
             <TextField
               id="standard-basic"
-              label="Standard"
+              label="Company Name"
               variant="standard"
-              style={{width: '100%'}}
+              autoComplete="password"
+              style={{ width: "100%" }}
             />
           </div>
           <div className="form-field">
             <TextField
               id="standard-basic"
-              label="Standard"
+              label="Your Name"
               variant="standard"
-              style={{width: '100%'}}
+              autoComplete="password"
+              style={{ width: "100%" }}
               required
             />
           </div>
           <div className="form-field">
             <TextField
               id="standard-basic"
-              label="Standard"
+              label="Your Email"
+              required
+              autoComplete="password"
+              type="email"
               variant="standard"
-              style={{width: '100%'}}
+              style={{ width: "100%" }}
             />
           </div>
           <div className="form-field">
             <TextField
               id="standard-basic"
-              label="Standard"
+              label="Your Phone Number"
+              autoComplete="password"
+              type="tel"
               variant="standard"
-              style={{width: '100%'}}
-            />
-          </div>
-          <div className="form-field">
-            <TextField
-              id="standard-basic"
-              label="Standard"
-              variant="standard"
-              style={{width: '100%'}}
+              style={{ width: "100%" }}
             />
           </div>
 
-          <TextField
-            className="form-field"
-            id="outlined-multiline-static"
-            label="Multiline"
-            multiline
-            rows={4}
-            defaultValue="Default Value"
-            style={{width: '100%', marginTop: '2rem'}}
-          />
+          <div className="form-field">
+            <TextField
+              className="form-field"
+              id="outlined-multiline-static"
+              label="Message"
+              multiline
+              rows={6}
+             
+              style={{ width: "100%", marginTop: "2rem" }}
+            />
+          </div>
+          <button className="form-btn">Submit</button>
         </div>
       </div>
     </div>
